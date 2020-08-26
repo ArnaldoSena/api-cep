bcdfcc9d940890eb74dd95e9a3a23d7210fc3498
7700411485 <daylon.costa-ext@viavarejo.com.br>
2020-08-26 20:44:48 -0300

    mapeamento de tabelas

--- src/main/java/br/com/cep/model/Cep.java
+++ src/main/java/br/com/cep/model/Cep.java
@@ -1,8 +1,20 @@
 package br.com.cep.model;
 
-@SuppressWarnings("unused")
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TCep")
 public class Cep {
 
+	@Id
 	private String cep;
 
 	private String logradouro;
@@ -11,6 +23,7 @@ public class Cep {
 
 	private String bairro;
 
+	@ManyToOne
 	private Cidade cidade;
 
 }
--- src/main/java/br/com/cep/model/Cidade.java
+++ src/main/java/br/com/cep/model/Cidade.java
@@ -1,15 +1,35 @@
 package br.com.cep.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.util.List;
-@SuppressWarnings("unused")
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TCidade")
 public class Cidade {
-  
+
+  @Id
   private String ibge;
   
   private String uf;
   
   private String localidade;
-  
+
+  @OneToMany(mappedBy = "cidade", cascade = CascadeType.ALL)
   private List<Cep> ceps; 
   
 }
